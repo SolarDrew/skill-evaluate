fbc048b641720e1ca43a754f604ad8672734e141
Drew Leonard <andy.j.leonard@gmail.com>
2017-10-11 16:21:27 +0100

    Include a setup function in case that's useful

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from opsdroid import match_regex
 
+def setup(opsdroid):
+    logging.debug("Loaded evaluate module")
+
 @match_regex('hi')
 async def hello(opsdroid, config, message):
     await message.respond('Hey')
